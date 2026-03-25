17318c952d07c06499ae53e41893d47b3d75093b
bainorann <romain.barriere@hotmail.fr>
2026-03-25 12:28:09 +0100

    test commit

---
+++ test.py
@@ -0,0 +1,55 @@
+print("hello world")
+# Source - https://stackoverflow.com/a/518007
+# Posted by Ryan Duffield, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-03-25, License - CC BY-SA 4.0
+
+import os
+import time
+clear = lambda: os.system('clear')
+clear()
+
+print(r"""\
+
+                                   ._ o o
+                                   \_`-)|_
+                                ,""       \ 
+                              ,"  ## |   ಠ ಠ. 
+                            ," ##   ,-\__    `.
+                          ,"       /     `--._;)
+                        ,"     ## /
+                      ,"   ##    /
+
+
+                """)
+time.sleep(2)
+clear()
+print(r"""\
+
+                                   ._ o o
+                                   \_`-)|_   * *
+                                ,""       \   * 
+                              ,"  ## |   - ಠ. 
+                            ," ##   ,-\__    `.
+                          ,"       /     `--._;)
+                        ,"     ## /
+                      ,"   ##    /
+
+
+                """)
+
+time.sleep(2)
+clear()
+print(r"""\
+
+                                   ._ o o
+                                   \_`-)|_
+                                ,""       \ 
+                              ,"  ## |   ಠ ಠ. 
+                            ," ##   ,-\__    `.
+                          ,"       /     `--._;)
+                        ,"     ## /
+                      ,"   ##    /
+
+
+                """)
+
